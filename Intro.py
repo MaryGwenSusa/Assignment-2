82a5d549042656666cdaf97001a62f8394a26b19
Mary Gwen Susa <Mary Gwen G. Susa>
2021-10-24 15:37:21 +0800

    Fixed errors for nth time

---
+++ Intro.py
@@ -0,0 +1,4 @@
+name = input("""Your Name:""")
+age = int(input("""Your Age:"""))
+location = input("""Your Location:""")
+print (f"Hi, my name is {name}. I am {age} years old and I live in {location}.")
